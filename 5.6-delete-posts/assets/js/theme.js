b6096ae82a44c8bb9e2dd5e854f943d265972b2d
Nair Designs` <nairbdg@gmail.com>
2022-05-18 18:06:58 +0800

    5.6 finished

--- 5.6-delete-posts/assets/js/theme.js
+++ 5.6-delete-posts/assets/js/theme.js
@@ -1,145 +1,127 @@
 (function () {
-
-    (function init() {
-
-        if ( true == jsforwp_vars.logged_in ) {
-            newPostBtn.addEventListener( 'click', togglePostForm );
-            savePostBtn.addEventListener( 'click', savePost );
-        }
-
-        wp.api.loadPromise.done( () => {
-            loadPosts();
-        } );
-
-    })();
-
-    function deletePost( event ) {
-
-        let title = event.target.parentElement.querySelector( '.entry-title a' ).innerText,
-            id = event.target.parentElement.dataset.id,
-            confirm = window.confirm( `Delete Post: "${title}"` );
-
-        event.preventDefault();
-
-        if ( true === confirm ) {
-
-            // Change POST_ID_OBJECT to '{ id }'
-            let post = new wp.api.models.Post( POST_ID_OBJECT );
-
-            // Change DELETE_POST() to post.destroy()
-            DELETE_POST()
-                .done( () => {
-                    loadMessage( 'deleted' );
-                    loadPosts();
-                } );
-        }
-
+  (function init() {
+    if (true == jsforwp_vars.logged_in) {
+      newPostBtn.addEventListener("click", togglePostForm);
+      savePostBtn.addEventListener("click", savePost);
     }
 
-    function loadEditForm( event ) {
+    wp.api.loadPromise.done(() => {
+      loadPosts();
+    });
+  })();
 
-        let title = event.target.parentElement.querySelector( '.entry-title a' ).innerText,
-            content = event.target.parentElement.querySelector( '.entry-content' ).innerHTML,
-            id = event.target.parentElement.dataset.id;
+  function deletePost(event) {
+    let title =
+        event.target.parentElement.querySelector(".entry-title a").innerText,
+      id = event.target.parentElement.dataset.id,
+      confirm = window.confirm(`Delete Post: "${title}"`);
 
-        event.preventDefault();
-        togglePostForm();
+    event.preventDefault();
 
-        formTitle.value = title;
-        tinyMCE.activeEditor.setContent( content );
-        savePostBtn.dataset.id = id;
+    if (true === confirm) {
+      // Change POST_ID_OBJECT to '{ id }'
+      let post = new wp.api.models.Post({ id });
 
+      // Change DELETE_POST() to post.destroy()
+      post.destroy().done(() => {
+        loadMessage("deleted");
+        loadPosts();
+      });
+    }
+  }
+
+  function loadEditForm(event) {
+    let title =
+        event.target.parentElement.querySelector(".entry-title a").innerText,
+      content =
+        event.target.parentElement.querySelector(".entry-content").innerHTML,
+      id = event.target.parentElement.dataset.id;
+
+    event.preventDefault();
+    togglePostForm();
+
+    formTitle.value = title;
+    tinyMCE.activeEditor.setContent(content);
+    savePostBtn.dataset.id = id;
+  }
+
+  function savePost(event) {
+    const newPost = {
+      title: formTitle.value,
+      content: tinyMCE.activeEditor.getContent(),
+      status: "publish",
+    };
+    let post = new wp.api.models.Post(newPost),
+      postId = savePostBtn.dataset.id,
+      currentPost = {};
+
+    if ("" !== postId) {
+      currentPost = { id: postId };
     }
 
-    function savePost( event ) {
+    event.preventDefault();
+    togglePostForm();
 
-        const newPost = {
-            'title': formTitle.value,
-            'content': tinyMCE.activeEditor.getContent(),
-            'status': 'publish'
-        };
-        let post = new wp.api.models.Post( newPost ),
-            postId = savePostBtn.dataset.id,
-            currentPost = {};
+    post.save(currentPost).done(() => {
+      loadMessage("saved");
+      clearForm();
+      loadPosts();
+    });
+  }
 
-        if ( '' !== postId ) {
-            currentPost = { id: postId };
-        }
+  function loadPosts() {
+    let posts = new wp.api.collections.Posts();
 
-        event.preventDefault();
-        togglePostForm();
+    posts
+      .fetch({
+        data: {
+          per_page: 3,
+        },
+      })
+      .done(() => {
+        clearPosts();
+        posts.each((post) => {
+          loadPost(post.attributes);
+        });
+      });
+  }
 
-        post.save( currentPost )
-            .done( () => {
-                loadMessage( 'saved' );
-                clearForm();
-                loadPosts();
-            } );
+  function loadPost(post) {
+    const article = document.createElement("article");
+    let markup = "";
 
+    markup += `<h2 class="entry-title"><a href="${post.link}">${post.title.rendered}</a></h2>`;
+    markup += `<div class="entry-content">${post.content.rendered}</div>`;
+    article.classList.add("post");
+    article.dataset.id = post.id;
+    article.innerHTML = markup;
+    if (true == jsforwp_vars.logged_in) {
+      article.append(getEditLink(), getDeleteLink());
     }
+    appContainer.append(article);
+  }
 
-    function loadPosts() {
+  function getEditLink() {
+    let link = document.createElement("a");
 
-        let posts = new wp.api.collections.Posts();
+    link.href = "#edit-post";
+    link.innerText = "Edit";
 
-        posts.fetch( {
-                data: {
-                    per_page: 3,
-                }
-            } )
-            .done( () => {
-                clearPosts();
-                posts.each( post => {
-                    loadPost( post.attributes );
-                } );
-            } );
+    link.addEventListener("click", loadEditForm);
 
-    }
+    return link;
+  }
 
-    function loadPost( post ) {
+  function getDeleteLink() {
+    let link = document.createElement("a");
 
-        const article = document.createElement( 'article' );
-        let markup = '';
+    link.href = "#delete-post";
+    link.innerText = "Delete";
+    link.classList.add("delete-post");
 
-        markup += `<h2 class="entry-title"><a href="${post.link}">${post.title.rendered}</a></h2>`;
-        markup += `<div class="entry-content">${post.content.rendered}</div>`;
-        article.classList.add( 'post' );
-        article.dataset.id = post.id;
-        article.innerHTML = markup;
-        if ( true == jsforwp_vars.logged_in ) {
-            article.append( getEditLink(), getDeleteLink() );
-        }
-        appContainer.append( article );
+    // Change DELETE_POST_FUNCTION to deletePost
+    link.addEventListener("click", deletePost);
 
-    }
-
-    function getEditLink() {
-
-        let link = document.createElement( 'a' );
-
-        link.href = '#edit-post';
-        link.innerText = 'Edit';
-
-        link.addEventListener( 'click', loadEditForm );
-
-        return link;
-
-    }
-
-    function getDeleteLink() {
-
-        let link = document.createElement( 'a' );
-
-        link.href = '#delete-post';
-        link.innerText = 'Delete';
-        link.classList.add( 'delete-post' )
-
-        // Change DELETE_POST_FUNCTION to deletePost
-        link.addEventListener( 'click', DELETE_POST_FUNCTION );
-
-        return link;
-
-    }
-
-
+    return link;
+  }
 })();
